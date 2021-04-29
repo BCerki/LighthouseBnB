5d468ebbdd012f92758d8d0f03673d76740865a9
Brianna Cerkiewicz <briannac@shaw.ca>
2021-04-29 21:59:34 +0000

    debug getUserWithEmail, getUserWithId, addUser to use the database

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -19,11 +19,14 @@ const users = require('./json/users.json');
  */
 const getUserWithEmail = function(email) {
   return pool
-  .query(`SELECT name FROM users
-  WHERE email = $1;`,[email])
-  .then((result) => result.rows)
-  .catch((err) => {
-    console.log(err.message);
+    .query(`SELECT * FROM users
+    WHERE email = $1;`,[email])
+    .then(result => {
+      console.log(result.rows[0])
+      return result.rows[0]
+    })
+    .catch(err => {
+      console.log(err.message);
   });
   
   // let user;
@@ -46,9 +49,9 @@ exports.getUserWithEmail = getUserWithEmail;
  */
 const getUserWithId = function(id) {
   return pool
-  .query(`SELECT name FROM users
+  .query(`SELECT * FROM users
   WHERE id = $1;`,[id])
-  .then((result) => result.rows)
+  .then((result) => result.rows[0])
   .catch((err) => {
     console.log(err.message);
   });
@@ -70,7 +73,10 @@ const addUser =  function(user) {
   .query(`INSERT INTO users (name, email, password)
   VALUES($1, $2, $3) 
   RETURNING *;`,[user.name, user.email, user.password])
-  .then((result) => result.rows)
+  .then((result) => {
+    console.log(result.rows[0])
+    return result.rows[0]
+  })
   .catch((err) => {
     console.log(err.message);
   });
@@ -93,7 +99,9 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+
+
+  // return getAllProperties(null, 2);
 }
 exports.getAllReservations = getAllReservations;
 
