0d18845e2d1995294d6495b0edf420a8af32e199
Brianna Cerkiewicz <briannac@shaw.ca>
2021-04-29 22:41:34 +0000

    add search filtering functionality to getAllProperties

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -123,12 +123,66 @@ exports.getAllReservations = getAllReservations;
  * @return {Promise<[{}]>}  A promise to the properties.
  */
 const getAllProperties = function(options, limit = 10) {
+
+const queryParams = [];
+//initial clauses
+
+let queryString = `
+SELECT properties.*, avg(property_reviews.rating) as average_rating
+FROM properties
+JOIN property_reviews ON properties.id = property_id
+`;
+
+
+//WHERE clauses
+if (options.city) 
+{
+  queryParams.push(`%${options.city}%`)
+  queryString += `WHERE city LIKE $${queryParams.length} `
+}
+
+if (options.owner_id) 
+{
+  queryParams.push(options.owner_id)
+  queryString += `AND owner_id = ${queryParams.length} `
+}
+
+if (options.minimum_price_per_night) 
+{
+  queryParams.push(Number(options.minimum_price_per_night)*100)
+  queryString += `AND cost_per_night >= $${queryParams.length} `
+}
+
+if (options.maximum_price_per_night) 
+{
+  queryParams.push(Number(options.maximum_price_per_night)*100)
+  queryString += `AND cost_per_night <= $${queryParams.length} `
+}
+
+if (options.minimum_rating) 
+{
+  queryParams.push(Number(options.minimum_rating))
+  queryString += `AND rating >= $${queryParams.length} `
+}
+
+//clauses that fall after where
+queryParams.push(limit);
+queryString += `
+  GROUP BY properties.id
+  ORDER BY cost_per_night
+  LIMIT $${queryParams.length};
+  `;
+
+
+
+console.log('queryString:',queryString, 'queryParams',queryParams)
+
   return pool
-  .query(`SELECT * FROM properties LIMIT $1`, [limit])
-  .then((result) => result.rows)
-  .catch((err) => {
-    console.log(err.message);
-  });
+    .query(queryString, queryParams)
+    .then(result => result.rows)
+    .catch((err) => {
+      console.log(err.message);
+   }); 
 
 
   // const limitedProperties = {};
